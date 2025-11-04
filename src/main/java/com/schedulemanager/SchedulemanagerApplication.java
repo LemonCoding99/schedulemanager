64a1a94c48ffccdc5c8706d0a3d87da57683ecd9
LemonCoding99 <qordmstj7530@gmail.com>
2025-11-04 10:55:14 +0900

    ✨ feat: 새로운 프로젝트 틀 작성

---
+++ src/main/java/com/schedulemanager/SchedulemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.schedulemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulemanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulemanagerApplication.class, args);
+    }
+
+}
