22c266991ed3a31a80b7fa34994191eab285673e
LemonCoding99 <qordmstj7530@gmail.com>
2025-11-05 16:54:53 +0900

    ✨ feat: 일정 수정 dto 클래스 작성

---
+++ src/main/java/com/schedulemanager/dto/UpdateScheduleRequestDto.java
@@ -0,0 +1,10 @@
+package com.schedulemanager.dto;
+
+import lombok.Getter;
+
+@Getter
+public class UpdateScheduleRequestDto {
+    private String title;
+    private String name;
+    private String password;
+}
