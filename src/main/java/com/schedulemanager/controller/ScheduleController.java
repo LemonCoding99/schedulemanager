e8ab5e83e6e65cd76d75e9e61e450dc14d13be9b
LemonCoding99 <qordmstj7530@gmail.com>
2025-11-05 17:33:17 +0900

    ♻ refactor: ResponseEntity 사용하여 코드 변경

--- src/main/java/com/schedulemanager/controller/ScheduleController.java
+++ src/main/java/com/schedulemanager/controller/ScheduleController.java
@@ -3,6 +3,8 @@ package com.schedulemanager.controller;
 import com.schedulemanager.dto.*;
 import com.schedulemanager.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,33 +16,38 @@ public class ScheduleController {
 
     // 일정 생성(Create)
     @PostMapping("/schedules")
-    public CreateScheduleResponseDto createSchedule(@RequestBody CreateScheduleRequestDto request) {
-        return scheduleService.save(request);
+    public ResponseEntity<CreateScheduleResponseDto> createSchedule(@RequestBody CreateScheduleRequestDto request) {
+        CreateScheduleResponseDto result = scheduleService.save(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
     }
 
     // 일정 전체 조회(Get)
     @GetMapping("/schedules")
-    public List<GetScheduleResponseDto> getSchedules() {
-        return scheduleService.getALL();
+    public ResponseEntity<List<GetScheduleResponseDto>> getAll() {
+        List<GetScheduleResponseDto> result = scheduleService.getALL();
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
     // 일정 단건 조회(Get)
     @GetMapping("/schedules/{scheduleId}")
-    public GetOneScheduleResponseDto getOneSchedule(@PathVariable Long scheduleId) {
-        return scheduleService.getOne(scheduleId);
+    public ResponseEntity<GetOneScheduleResponseDto> getOne(@PathVariable Long scheduleId) {
+        GetOneScheduleResponseDto result = scheduleService.getOne(scheduleId);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
     // 일정 수정(Update)
     @PutMapping("/schedules/{scheduleId}")
-    public UpdateScheduleResponseDto update(
+    public ResponseEntity<UpdateScheduleResponseDto> update(
             @PathVariable Long scheduleId,  // URL 경로에 있는 scheduleId 값을 메서드 매개변수로 주입
             @RequestBody UpdateScheduleRequestDto request  // JSON을 UpdateScheduleRequestDto 객체로 변환
     ) {
-        return scheduleService.update(scheduleId, request);
+        UpdateScheduleResponseDto result = scheduleService.update(scheduleId, request);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
     @DeleteMapping("schedules/{scheduleId}")
-    public void delete(@PathVariable Long scheduleId) {
+    public ResponseEntity<Void> delete(@PathVariable Long scheduleId) {
         scheduleService.delete(scheduleId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
